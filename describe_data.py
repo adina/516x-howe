b4a2ca6cbd84a2416519edb8590c799d3c47bf5b
EC2 Default User <ec2-user@ip-172-31-21-171.ec2.internal>
2019-09-06 14:05:47 +0000

    updated script

--- describe_data.py
+++ describe_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import sys
 
-url = "https://raw.githubusercontent.com/isu-abe/516x/master/data/surveys1977.csv"
-df = pd.read_csv(url, sep = ",", index_col=0)
+file_to_open = open(sys.argv[1], 'r')
+#url = "https://raw.githubusercontent.com/isu-abe/516x/master/data/surveys1977.csv"
+df = pd.read_csv(file_to_open, sep = ",", index_col=0)
 print(df.describe())
